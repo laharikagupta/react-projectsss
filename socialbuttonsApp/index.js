13dc44a3370ea0e413f6d38642eba30551cbf858
laharikagupta <racharlalaharika@gmail.com>
2024-06-11 06:15:26 +0000

    social buttons correct code

--- socialbuttonsApp/index.js
+++ socialbuttonsApp/index.js
@@ -1,20 +1,19 @@
 const Button = props => {
-  //  Write your code here.
-  {buttontext,classname}=props;
-  <button className={`${classname}`}>{buttontext}</button>
+  const {className, buttonText} = props
+  
+  return <button className={`button ${className}`}>{buttonText}</button>
 }
 
 const element = (
-  //  Write your code here.
-  <div>
-  <div className="background">
-      <h1>Social Buttons App</h1>
-      <div className="container">
-          <Button buttontext="Like" classname="b"/>
-          <Button buttontext="Comment" classname="b2"/>
-          <Button buttontext="Share" classname="b3"/>
+  <div className="social-buttons-container">
+    <div className="content-container">
+      <h1 className="heading">Social Buttons</h1>
+      <div className="buttons-container">
+        <Button buttonText="Like" className="like-button" />
+        <Button buttonText="Comment" className="comment-button" />
+        <Button buttonText="Share" className="share-button" />
       </div>
-  </div>
+    </div>
   </div>
 )
 
